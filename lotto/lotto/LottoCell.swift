e8bd56c6ec9520fb62bbaa5c9011cb7479d31b17
Woopa01 <whdnwls49@naver.com>
2018-11-14 17:08:25 +0900

    add draw func

---
+++ lotto/lotto/LottoCell.swift
@@ -0,0 +1,19 @@
+//
+//  LottoCell.swift
+//  lotto
+//
+//  Created by 조우진 on 14/11/2018.
+//  Copyright © 2018 조우진. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LottoCell : UITableViewCell {
+    @IBOutlet weak var number1: UILabel!
+    @IBOutlet weak var number2: UILabel!
+    @IBOutlet weak var number3: UILabel!
+    @IBOutlet weak var number4: UILabel!
+    @IBOutlet weak var number5: UILabel!
+    @IBOutlet weak var number6: UILabel!
+}
--- lotto/lotto/ViewController.swift
+++ lotto/lotto/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController,UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    var lottoNumbers = Array<Array<Int>>()
+
+    @IBAction func doDraw(_ sender: Any) {
+        lottoNumbers = Array<Array<Int>>()
+        
+        var originalNumbers = Array(1 ... 45)
+        var index = 0
+        
+        for _ in 0 ... 4{
+            originalNumbers = Array(1...45)
+            var columnArray = Array<Int>()
+            
+            for _ in 0 ... 5{
+                index = Int(arc4random_uniform(UInt32(originalNumbers.count)))
+                columnArray.append(originalNumbers[index])
+                originalNumbers.remove(at: index)
+            }
+            columnArray.sort(by: {$0 < $1})
+            lottoNumbers.append(columnArray)
+        }
+        
+        tableView.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return lottoNumbers.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "lottoCell", for: indexPath as IndexPath) as! LottoCell
+        
+        let row = indexPath.row
+        
+        cell.number1.text = "\(lottoNumbers[row][0])"
+        cell.number2.text = "\(lottoNumbers[row][1])"
+        cell.number3.text = "\(lottoNumbers[row][2])"
+        cell.number4.text = "\(lottoNumbers[row][3])"
+        cell.number5.text = "\(lottoNumbers[row][4])"
+        cell.number6.text = "\(lottoNumbers[row][5])"
+        
         return cell
     }
     
