0ba2299f1b10bbec2e776e1cff166a6652409b96
Woopa01 <whdnwls49@naver.com>
2018-11-19 09:05:09 +0900

    add DB

--- lotto/lotto/ViewController.swift
+++ lotto/lotto/ViewController.swift
@@ -11,6 +11,36 @@ import UIKit
 class ViewController: UIViewController,UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     var lottoNumbers = Array<Array<Int>>()
+    var databasePath = String()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fileMgr = FileManager.default
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        
+        let docsDir = dirPaths[0]
+        databasePath = docsDir + "/lotto.db"
+        
+        if !fileMgr.fileExists(atPath: databasePath as String){
+            let db : FMDatabase? = FMDatabase(path: databasePath as String)
+            
+            if db == nil{
+                NSLog("db 생성 오류")
+            }
+            
+            if ((db?.open()) != nil){
+                let sql_statment = "Create table if not exists lotto( id integer primary key autoincrement, number1 integer, number2 integer, number3 integer, number4 integer, number5 integer, number6 integer)"
+                
+                db?.executeQuery(sql_statment, withArgumentsIn: ["10"])
+            }
+            
+            db?.close()
+        } else {
+            NSLog("db 연결오류")
+        }
+    }
+
 
     @IBAction func doDraw(_ sender: Any) {
         lottoNumbers = Array<Array<Int>>()
@@ -33,6 +63,46 @@ class ViewController: UIViewController,UITableViewDataSource {
         
         tableView.reloadData()
     }
+    
+    @IBAction func doSave(_ sender: Any) {
+        let db : FMDatabase? = FMDatabase(path: databasePath as String)
+        
+        if db?.open() != nil {
+            db?.executeQuery("delete from lotto", withArgumentsIn: ["10"])
+
+            for numbers in lottoNumbers{
+                let insertQuery = "insert into lotto(number1, number2, number3, number4, number5, number6) values (\(numbers[0]), \(numbers[1]), \(numbers[2]), \(numbers[3]), \(numbers[4]), \(numbers[5])"
+                
+                db?.executeQuery(insertQuery, withArgumentsIn: ["10"])
+            }
+        }
+    }
+    
+    @IBAction func doLoad(_ sender: Any) {
+        lottoNumbers = Array<Array<Int>>()
+        let db : FMDatabase? = FMDatabase(path: databasePath as String)
+        
+        if db?.open() != nil{
+            let selectQuery = "select number1, number2, number3, number4, number5, number6 from lotto"
+            let result : FMResultSet? =
+                db?.executeQuery(selectQuery, withArgumentsIn: ["10"])
+            
+            if result != nil{
+                while result!.next(){
+                    var columArray = Array<Int>()
+                    columArray.append(Int(result!.string(forColumn: "number1") ?? " " )!)
+                    columArray.append(Int(result!.string(forColumn: "number2") ?? " " )!)
+                    columArray.append(Int(result!.string(forColumn: "number3") ?? " " )!)
+                    columArray.append(Int(result!.string(forColumn: "number4") ?? " " )!)
+                    columArray.append(Int(result!.string(forColumn: "number5") ?? " " )!)
+                    columArray.append(Int(result!.string(forColumn: "number6") ?? " " )!)
+                    lottoNumbers.append(columArray)
+                }
+            }
+        }
+        tableView.reloadData()
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return lottoNumbers.count
@@ -57,12 +127,5 @@ class ViewController: UIViewController,UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
 }
 
